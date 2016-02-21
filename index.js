3030ba193afdbeb64f000e7be6c456789af0c095
Asbjørn Thegler <asbjoern@gmail.com>
2016-02-21 01:22:11 +0100

    Initial commit
    Lots of tests missing, yet

---
+++ index.js
@@ -0,0 +1,10 @@
+var log = require("bunyan").createLogger({ name: "strike-list", src: true });
+
+var config = require("config");
+log.info("Current environment is '" + config.util.getEnv("NODE_ENV") + "'");
+
+var Application = require("./lib/Application.js");
+
+var appliation = new Application(config, log);
+
+appliation.start();
---
+++ spec/endpoints/items/getItemSpec.js
@@ -0,0 +1,33 @@
+var getItem = require("../../../lib/endpoints/items/getItem.js");
+
+describe("getItem", () => {
+    it("works", () => {
+        var item = { name: "Beer", price: 1000 };
+        var req = {
+            log: {
+                info: jasmine.createSpy("req.log.info"),
+                warn: jasmine.createSpy("req.log.warn"),
+                error: jasmine.createSpy("req.log.error")
+            },
+            item: item
+        };
+
+        var res = {
+            status: jasmine.createSpy("res.status").and.callFake(function() {
+                return res;
+            }),
+            send: jasmine.createSpy("res.send").and.callFake(function() {
+                return res;
+            })
+        };
+
+        getItem(req, res);
+
+        expect(req.log.info).toHaveBeenCalled();
+        expect(req.log.warn).not.toHaveBeenCalled();
+        expect(req.log.error).not.toHaveBeenCalled();
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith({ item: item });
+    });
+});
